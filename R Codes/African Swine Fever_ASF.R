a8a9a365714a85eb1bcd82d4dbb4c4f0c694a752
Ofosuhene O. Apenteng <142171557+ofosuheneapenteng@users.noreply.github.com>
2024-04-28 21:13:32 +0200

    Update African Swine Fever_ASF.R

--- R Codes/African Swine Fever_ASF.R
+++ R Codes/African Swine Fever_ASF.R
@@ -165,7 +165,22 @@ WOEM_pigs <- WOEM_pigs[!WOEM_pigs$country %in% RemoveCountries, ]
 RemoveCountries <- c("Ukraine","Russia", "Belarus")
 WOEM_boar<- WOEM_boar[!WOEM_boar$country %in% RemoveCountries, ]
 
-#------------------------------------------------------
+#------Do not run------------------------------------------------
+
+#tif_map_world <- raster("5_Pg_2015_Da.tif")
+# for pigs and the user can the same link get the rest of the population densities as stated in the 
+#README or in the code. 
+
+#plot(tif_map_world)
+
+#tif_map_eu_LIM <- extent(-10, 40, 30, 70)
+
+#subset_raster <- raster(ext = tif_map_eu_LIM,
+ #                       nrows = nrow(tif_map_world),
+  #                      ncols = ncol(tif_map_world),
+   #                     crs = projection(tif_map_world))
+#stock <- crop(tif_map_world, subset_raster)
+#--------------End of Do not run----------------------------------------
 #Read in the bio climate and land cover data
 prec<- rast("bio_12.tif") #Precipitation
 temp<- rast("bio_1.tif") # Temperature
